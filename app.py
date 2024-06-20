5e1c12c0bc27c8519ea14f25b0f9c58da1e58ba3
John Mendez <66302608+johnmendez2@users.noreply.github.com>
2024-06-20 18:02:35 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -705,7 +705,7 @@ def send_callback(user_id, task_id,requestId, processing_duration, data):
     time.sleep(2)
 
     response = requests.post(webhook_url, json=callback_message, headers=headers)
-
+    print(response)
 ############### RUN YOUR SERVER HERE ###############
 if __name__ == '__main__':
     app.run(debug=True)
